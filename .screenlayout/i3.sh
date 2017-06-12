673b9f5564273c8995a9c2ba6718fae7220dfb15
Jim Yang <jim4nz@gmail.com>
2017-06-12 23:46:52 +1200

    ubuntu -i3 build

---
+++ .screenlayout/i3.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1 --mode 1920x1200 --pos 0x0 --rotate left --output HDMI-1 --off --output eDP-1 --primary --mode 3200x1800 --pos 1200x0 --rotate normal --output HDMI-2 --off
---
+++ .screenlayout/single.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1200x0 --rotate normal --output HDMI-2 --off
