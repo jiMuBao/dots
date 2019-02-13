c7d98222314c8f105b27e8675b6aa8cbe0c812c7
Jim Yang <jim4nz@gmail.com>
2019-02-14 11:59:49 +1300

    update polybar

--- .screenlayout/work.sh
+++ .screenlayout/work.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-1 --mode 1920x1080 --pos 1080x0 --rotate normal --output eDP-1 --primary --scale 0.5x0.5 --mode 3200x1800 --pos 3000x0 --rotate normal --output HDMI-2 --off
+# xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate left --output
+#  HDMI-1 --mode 1920x1080 --pos 1080x0 --rotate normal --output 
+# eDP-1 --primary --scale 0.5x0.5 --mode 3200x1800 --pos 3000x0 --rotate normal 
+# --output HDMI-2 --off
+
+xrandr \
+    --output DP-1 --mode 1920x1080 --rotate left --left-of HDMI-1 \
+    --output HDMI-1 --mode 1920x1080 --rotate normal \
+    --output eDP-1 --primary --scale 0.5x0.5 --mode 3200x1800 --right-of HDMI-1
